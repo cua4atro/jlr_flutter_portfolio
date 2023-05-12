01bfe1b5658aa3967fe8a1258d8d54550005429b
jlreartes <reartes.j.l@gmail.com>
2023-05-12 05:07:02 -0300

    set only darktheme until config

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -59,9 +59,9 @@ class MyApp extends StatelessWidget {
           // Define a light and dark color theme. Then, read the user's
           // preferred ThemeMode (light, dark, or system default) from the
           // SettingsController to display the correct theme.
-          theme: ThemeData(),
-          darkTheme: ThemeData.dark(),
-          themeMode: settingsController.themeMode,
+          theme: ThemeData.dark(),
+          //darkTheme: ThemeData.dark(),
+          //themeMode: settingsController.themeMode,
 
           // Define a function to handle named routes in order to support
           // Flutter web url navigation and deep linking.
